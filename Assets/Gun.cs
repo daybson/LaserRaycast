84656d517067bbd2821fab844af4f57b6ea7b2a1
unknown <daybson.paisante@TSABHZANB0120.tsa.local>
2014-11-28 22:12:08 -0200

    Initial commit

---
+++ Assets/Gun.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright 2014
+ * autor: Daybson B.S. Paisante
+ * email: daybsonbsp@gmail.com
+ * encontrado em: http://daybsonpaisante.wordpress.com/ 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+		private LineRenderer lineRenderer;
+
+		void Start ()
+		{   
+				var MyNewVar = this.transform.GetComponent<Rigidbody> ();
+				//salva referência ao LineRenderer
+				lineRenderer = this.GetComponent<LineRenderer> ();
+		}
+
+		void Update ()
+		{
+				//objeto do contato do raio lançado
+				RaycastHit hit;
+
+				//origem da linha
+				lineRenderer.SetPosition (0, this.transform.position);
+
+				//se raio tocar em algo, hit é preenchido com informações do contato
+				if (Physics.Raycast (this.transform.position, this.transform.forward, out hit))
+            
+            //seta final da linha no ponto de contato
+						lineRenderer.SetPosition (1, hit.point);
+				else
+            //seta final da linha rumo ao infinito na direção frontal do player.
+						lineRenderer.SetPosition (1, transform.forward * 99999999);
+		}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright 2014
+ * autor: Daybson B.S. Paisante
+ * email: daybsonbsp@gmail.com
+ * encontrado em: http://daybsonpaisante.wordpress.com/ 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    //fator de movimentação
+    public float speed;
+
+    private void Update()
+    {
+        //rotaciona e translada de acordo com o input do usuário
+        transform.Rotate(0, Input.GetAxis("Horizontal") * speed, 0);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+}
