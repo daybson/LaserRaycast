c574f1cf9d45040fd917f5a00cdb094a671bc1e5
Daybson Paisante <daybson@users.noreply.github.com>
2015-01-15 07:16:21 -0200

    Update Movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * Copyright 2014
- * autor: Daybson B.S. Paisante
- * email: daybsonbsp@gmail.com
- * encontrado em: http://daybsonpaisante.wordpress.com/ 
+ * author: Daybson B.S. Paisante
+ * daybson.paisante@outlook.com
+ * http://daybsonpaisante.wordpress.com/ 
  */
 
 using UnityEngine;
@@ -10,12 +10,11 @@ using System.Collections;
 
 public class Movement : MonoBehaviour
 {
-    //fator de movimentação
     public float speed;
 
     private void Update()
     {
-        //rotaciona e translada de acordo com o input do usuário
+        //rotate and translate by user input
         transform.Rotate(0, Input.GetAxis("Horizontal") * speed, 0);
         transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
     }
