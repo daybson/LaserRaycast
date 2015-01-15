2bc0a173a9c9641cce56e687fcfda06f76910b6f
Daybson Paisante <daybson@users.noreply.github.com>
2015-01-15 07:15:25 -0200

    Update Gun.cs

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * Copyright 2014
- * autor: Daybson B.S. Paisante
- * email: daybsonbsp@gmail.com
- * encontrado em: http://daybsonpaisante.wordpress.com/ 
+ * author: Daybson B.S. Paisante
+ * daybson.paisante@outlook.com
+ * http://daybsonpaisante.wordpress.com/ 
  */
 
 using UnityEngine;
@@ -10,30 +10,24 @@ using System.Collections;
 
 public class Gun : MonoBehaviour
 {
-		private LineRenderer lineRenderer;
-
+	private LineRenderer lineRenderer;
 		void Start ()
-		{   
-				var MyNewVar = this.transform.GetComponent<Rigidbody> ();
-				//salva referência ao LineRenderer
-				lineRenderer = this.GetComponent<LineRenderer> ();
-		}
-
+	{   
+		lineRenderer = this.GetComponent<LineRenderer> ();
+	}
 		void Update ()
-		{
-				//objeto do contato do raio lançado
-				RaycastHit hit;
-
-				//origem da linha
-				lineRenderer.SetPosition (0, this.transform.position);
-
-				//se raio tocar em algo, hit é preenchido com informações do contato
-				if (Physics.Raycast (this.transform.position, this.transform.forward, out hit))
-            
-            //seta final da linha no ponto de contato
-						lineRenderer.SetPosition (1, hit.point);
-				else
-            //seta final da linha rumo ao infinito na direção frontal do player.
-						lineRenderer.SetPosition (1, transform.forward * 99999999);
-		}
+	{
+		RaycastHit hit;
+		
+		//line origin
+		lineRenderer.SetPosition (0, this.transform.position);
+		
+		//if the ray hits something, fill hit with the contact point informations
+		if (Physics.Raycast (this.transform.position, this.transform.forward, out hit))
+                   	//line ending is set as the contact point
+			lineRenderer.SetPosition (1, hit.point);
+		else
+           		//if hits anything, line goes to infinity foward the player
+			lineRenderer.SetPosition (1, transform.forward * 99999999);
+	}
 }
